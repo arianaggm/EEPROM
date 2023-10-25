2fefa29e04d3138f81e82908d34604a30e41529d
arianaggm <144489620+arianaggm@users.noreply.github.com>
2023-10-24 23:48:19 -0600

    Create eeprom.h

---
+++ eeprom.h
@@ -0,0 +1,16 @@
+#ifndef eeprom_h
+#define	eeprom_h
+
+int contador;
+
+char leeEE(char direc);
+int escribeEE(char direc, char escritura);
+void configUART(void);
+void TXsend(char dato);
+void TXstringln(int length, char s[]);
+void TXnumber(int number);
+void TXln(void);
+void configInterrupts(void);
+void readSerial(int size);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
