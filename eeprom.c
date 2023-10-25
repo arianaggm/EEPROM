3e803107670ca183224ee7813ec3fee923a607dc
arianaggm <144489620+arianaggm@users.noreply.github.com>
2023-10-24 23:47:32 -0600

    Update eeprom.c

--- eeprom.c
+++ eeprom.c
@@ -1,186 +1,110 @@
 #include <xc.h>
-#include "config.h"
-#include "LCD_PORTD.h"
+#include "eeprom.h"
 
-#define PWM_FREQ 2500
-#define PWM1_out TRISCbits.RC2
-#define PWM2_out TRISCbits.RC1
-#define PWM3_out TRISCbits.RC0
-#define PWM3_pin LATCbits.LC0
+void configUART(){
+     //----Config UART (serial port) ----
+     BAUDCONbits.BRG16 = 0; //8 bits mode
+     TXSTAbits.BRGH = 0; //Low speed
+     TXSTAbits.SYNC = 0; //Asynchronous mode
+     //Baudrate = Fosc/(64(spbrg + 1))
+     //9615 = 8MHz/(64(12+1))
+     SPBRG = 12; //Config baud-rate at 9600
+     BAUDCONbits.TXCKP = 0; //Polarity NON inverted
+     BAUDCONbits.RXDTP = 0;
+     TXSTAbits.TX9 = 0; //Transmit 8 bits
+     RCSTAbits.RX9 = 0; //Reception 8 bits
+     TXSTAbits.TXEN = 1; //Enable transmision
+     RCSTAbits.CREN = 1; //Enable reception
+     RCSTAbits.SPEN = 1; //Enable serial port
+     //-------------------------
+}
 
-#define IN 1
-#define OUT 0
-#define ON 1
-#define OFF 0
-#define LOW 0
-#define HI 1
-
-void configADC (char CHANNELS);
-unsigned int analogRead (char canal);
-void configPWM1(void);
-void configPWM2(void);
-void configPWM3(void);
-void setDC1(char x);
-void setDC2(char x);
-void setDC3(char x);
-
-char duty3;
-char HiLo;
-int preLoad;
-double multiplier = _XTAL_FREQ/(PWM_FREQ*16.0*100.0);
-
-void main (void){
-    OSCCON = 127;
-    iniLCD();
-    LCDcommand(DispOn);
-    LCDprint(9, "LECTURA: ", 100);
-    configADC(3); //Configs 3 ADC channels
-    unsigned int lec1;
-    unsigned int lec2;
-    unsigned int lec3;
-    double percent1;
-    double percent2;
-    double percent3;
-    configPWM1();
-    configPWM2();
-    configPWM3();
-    while(1){
-        MoveCursor(0, LINE_DOWN);
-        lec1 = analogRead(0);
-        lec2 = analogRead(1);
-        lec3 = analogRead(2);
-        percent1 = 100.0*lec1/1023.0;
-        percent2 = 100.0*lec2/1023.0;
-        percent3 = 100.0*lec3/1023.0;
-        setDC1((int)percent1);
-        setDC2((int)percent2);
-        setDC3((int)percent3);
-        LCDint(lec1);
-        LCDchar(' ');
-        LCDint(lec2);
-        LCDchar(' ');
-        LCDint(lec3);
-        LCDchar(' ');
-        LCDprint(9, "          ", 0);
-        __delay_ms(400);
-        MoveCursor(0, LINE_DOWN);
-        LCDint((int)percent1);
-        LCDchar('%');
-        LCDchar(' ');
-        LCDint((int)percent2);
-        LCDchar('%');
-        LCDchar(' ');
-        LCDint((int)percent3);
-        LCDchar('%');
-        LCDchar(' ');
-        LCDprint(9, "          ", 0);
-        __delay_ms(400);
+void TXsend(char dato){
+    while(TXSTAbits.TRMT == 0){
+        //Wait until transmit register is available
     }
-    return;
+    TXREG = dato;
 }
 
-void configADC(char CHANNELS){
-    char allchannelsoff = 0x0F;
-    ADCON1 = (allchannelsoff-CHANNELS);
-    ADCON2 = 0x80; //Right justified
-    ADCON0bits.ADON = 1; //Turn on AD Port
-}
-
-unsigned int analogRead (char canal){
-    ADCON0 = 1+canal*4;
-     ADCON0bits.GO_DONE = 1; //start conversion
-     while(ADCON0bits.GO_DONE == 1) {
-          //wait for conversion to finish
-      }
-     char lowbits = ADRESL; //read results
-     char highbits = ADRESH;
-     unsigned int adresult = highbits*256+ lowbits;
-     return adresult;
-}
-
-void configPWM1(){
-    PWM1_out = OUT;
-    PR2 = _XTAL_FREQ/(4.0*PWM_FREQ*16.0)-1; 
-    //PR2 = 249; //Value to generate 500Hz --> Period=2ms
-    setDC1(50);
-    T2CON = 0b00000111; //T2 ON, <1:0>prescaler = 16
-    CCP1CON = 0b00001100; //<5:4>=<00> PWM mode
-//    CCPR1L = 0b01111101; //DC (1 ms)00
-//    CCP1CONbits.DC1B0=0;
-//    CCP1CONbits.DC1B1=0; //<5:4>=<00>
-//    TRISC = 0x00; //PortC output
-     return;
-}
-
-void configPWM2(){
-    PWM2_out = OUT;
-    setDC2(50);
-    CCP2CON = 0b00001100; //<5:4>=<00> PWM mode
-//     PR2 = 249; //Value to generate 500Hz --> Period=2ms
-//     CCPR2L = 0b01111101; //DC (1 ms)00
-//     CCP2CONbits.DC1B0=0;
-//     CCP2CONbits.DC1B1=0; //<5:4>=<00>
-//     TRISC = 0x00; //PortC output
-//     T2CON = 0b00000111; //T2 ON, <1:0>prescaler = 16
-     
-     return;
-}
-
-void configPWM3(){
-    PWM3_out = OUT; //Salida del PWM3
-    setDC3(50); //Configura DC del PWM3
-    INTCONbits.TMR0IF = 0;
-    INTCONbits.T0IE = 1; //Habilita la interrupciones del
-    INTCONbits.GIE = 1; //Habilita la interrupciones
-    T0CON = 0b00001000; //16 bits<6> preescalador OFF<3>
-    //Preecarga = 65535 - DC*400 (50 Hz)
-    HiLo = 0;
-    PWM3_pin = HiLo; //Genera la salida en 
-    TMR0 = 45535; //DC 50%
-    T0CONbits.TMR0ON = 1;
-     return;
-}
-
-void setDC1(char x){
-    if(x > 100){
-        x = 100;
+void TXstring(int length, char s[]){
+    for(int i = 0; i < length; i++){
+        TXsend(s[i]);
     }
-    int val = x*multiplier;  //Load for DC
-    CCPR1L = (val - val%4)/4;  //8 MSB <10:2>
-    CCP1CONbits.DC1B = val%4;  //2 LSB   <1:9>
 }
 
-
-void setDC2(char x){
-    if(x > 100){
-        x = 100;
+void TXstringln(int length, char s[]){
+    for(int i = 0; i < length; i++){
+        TXsend(s[i]);
     }
-    int val = x*multiplier;  //Load for DC
-    CCPR2L = (val - val%4)/4;  //8 MSB <10:2>
-    CCP2CONbits.DC2B = val%4;  //2 LSB   <1:9>
+    TXsend(10); //Line Feed
+    TXsend(13); //CR
 }
 
-void setDC3(char x){
-    if(x > 100){
-        x = 100;
-    }
-    duty3 = x; //Para las interrupciones
+void TXnumber(int number){
+     char units=0;
+     char decs=0;
+     char cents=0;
+     char mils=0;
+     units = number%10;
+     decs = (number%100-units)/10;
+     cents = (number%1000 - decs*10 - units)/100;
+     mils = (number%10000 - cents*100 - decs*10 - units)/1000;
+     if(number > 999){
+          TXsend('0'+mils);
+     }
+     if(number > 99){
+         TXsend('0'+cents);
+     }
+     if(number > 9){
+          TXsend('0'+decs);
+     }
+     TXsend('0'+units);
 }
 
-void __interrupt () miISR (){ //Interupt Service Routine
-    if (INTCONbits.T0IF==1){ //Checa si el timer0 terminó de contar
-        unsigned int preLoad ;
-        PWM3_pin = HiLo;
-        if (HiLo==1){
-            preLoad = 65535 - (duty3)*400;
-            HiLo = 0;
-        }
-        else{
-            preLoad = 65535 - (100-duty3)*400;
-            HiLo = 1;
-        }
-        TMR0 = preLoad;
-        INTCONbits.T0IF = 0; //Baja la bandera del timer 0
-    }
+void configInterrupts(){
+     INTCONbits.GIE = 1; //Habilita interrupciones globles
+     INTCONbits.PEIE = 1; //Habilita interrupciones periféricas
+     PIR1bits.RCIF = 0; //baja la bandera RC
+     PIE1bits.RCIE = 1; //Habilia la interrupción RC
+     RCONbits.IPEN = 0; //No priorities
+}
 
+void readSerial(int size){
+     contador = 0;
+     while(contador < size){
+
+     }
+}
+
+char leeEE(char direc){
+   char lectura;
+   EEADR = direc;
+   EECON1bits.EEPGD = 0;
+   EECON1bits.CFGS = 0;
+   EECON1bits.RD = 1;
+   lectura = EEDATA;
+   return lectura;
+}
+
+int escribeEE(char direc, char escritura){
+  int error = 0;
+  EEADR = direc;
+  EEDATA = escritura;
+  EECON1bits.EEPGD = 0;
+  EECON1bits.CFGS = 0;
+  EECON1bits.WREN = 1;
+  //<-- disable interrupts here
+   EECON2 = 0x55;
+   EECON2 = 0xAA;
+   EECON1bits.WR = 1;
+   //Enable Interrupts here
+   while(PIR2bits.EEIF != 1){
+
+   }
+   PIR2bits.EEIF = 0;
+   EECON1bits.WREN = 0;
+   if(EECON1bits.WRERR==1){
+      error = 1;
+   }
+   return error;
 }
